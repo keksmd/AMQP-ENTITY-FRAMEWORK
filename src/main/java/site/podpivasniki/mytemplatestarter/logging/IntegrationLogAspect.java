f243da8ee3fce942e2a15bf7266a539fd683c51c
xzz1p <olezhka.kirilov.97@mail.ru>
2025-02-08 14:05:03 +0300

    фиксы

--- src/main/java/site/podpivasniki/mytemplatestarter/logging/IntegrationLogAspect.java
+++ src/main/java/site/podpivasniki/mytemplatestarter/logging/IntegrationLogAspect.java
@@ -2,7 +2,6 @@ package site.podpivasniki.mytemplatestarter.logging;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.time.Instant;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
