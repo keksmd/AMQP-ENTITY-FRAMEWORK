9163bfa7df4a22f707d7d4838b85cfbb38cda798
alex <lexagri200430@gmail.com>
2025-02-18 09:27:36 +0300

    next snapshot ver

--- src/main/java/dada/tuda/framework/configuration/aspect/publiced/EnumBeanConfiguration.java
+++ src/main/java/dada/tuda/framework/configuration/aspect/publiced/EnumBeanConfiguration.java
@@ -1,11 +1,14 @@
 package dada.tuda.framework.configuration.aspect.publiced;
 
 import dada.tuda.framework.annotations.EnumBean;
+import org.jetbrains.annotations.NotNull;
 import org.reflections.Reflections;
 import org.reflections.scanners.Scanners;
 import org.reflections.util.ClasspathHelper;
 import org.reflections.util.ConfigurationBuilder;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.support.AbstractBeanDefinition;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
 import org.springframework.beans.factory.support.RootBeanDefinition;
 import org.springframework.boot.autoconfigure.AutoConfigureOrder;
@@ -22,15 +25,18 @@ import java.util.stream.Collectors;
 public class EnumBeanConfiguration {
     @Bean
     static BeanDefinitionRegistryPostProcessor enumBeanRegistrar() {
-        return registry -> {
+        return new EnumBeanRegistrar();
+    }
+
+    private static class EnumBeanRegistrar implements BeanDefinitionRegistryPostProcessor, Ordered {
+        @Override
+        public void postProcessBeanDefinitionRegistry(@NotNull BeanDefinitionRegistry registry) throws BeansException {
             Collection<URL> urls = ClasspathHelper.forJavaClassPath();
             Set<URL> filteredUrls = urls.stream()
                     // Фильтруем по каким-то условиям
                     .filter(url -> !url.getFile().contains("spring") && !url.toString().contains("hibernate-") && !url.toString().contains("jakarta") && !url.toString().contains("javax")&& !url.toString().contains("java"))
                     // Можно также проверять url.getPath() или url.toString()
                     .collect(Collectors.toSet());
-
-
             Reflections reflections = new Reflections(new ConfigurationBuilder()
                     .setUrls(filteredUrls)
                     .setScanners(Scanners.TypesAnnotated));Set<Class<?>> annotatedClasses = reflections.getTypesAnnotatedWith(EnumBean.class);
@@ -53,10 +59,14 @@ public class EnumBeanConfiguration {
                         definition.setAutowireMode(AbstractBeanDefinition.AUTOWIRE_NO);
                         registry.registerBeanDefinition((hasPrefix ? (clazz.getSimpleName() + ".") : "") + enumConst.name(), definition);
                     }
-
                 }
             }
-        };
+        }
+
+        @Override
+        public int getOrder() {
+            return Ordered.HIGHEST_PRECEDENCE;
+        }
     }
 }
 
