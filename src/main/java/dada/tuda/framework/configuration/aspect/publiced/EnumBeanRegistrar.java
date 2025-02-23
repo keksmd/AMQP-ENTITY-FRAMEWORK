094752141e4b5c9458d93d43ca4316f1e961291d
alex <lexagri200430@gmail.com>
2025-02-24 01:39:03 +0300

    next snapshot ver

--- src/main/java/dada/tuda/framework/configuration/aspect/publiced/EnumBeanRegistrar.java
+++ src/main/java/dada/tuda/framework/configuration/aspect/publiced/EnumBeanRegistrar.java
@@ -1,64 +1,73 @@
 package dada.tuda.framework.configuration.aspect.publiced;
 
 import dada.tuda.framework.annotations.EnumBean;
-import org.jetbrains.annotations.NotNull;
-import org.reflections.Reflections;
-import org.reflections.scanners.Scanners;
-import org.reflections.util.ClasspathHelper;
-import org.reflections.util.ConfigurationBuilder;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.support.AbstractBeanDefinition;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.support.RootBeanDefinition;
 import org.springframework.context.annotation.ImportBeanDefinitionRegistrar;
-import org.springframework.core.Ordered;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.core.type.AnnotationMetadata;
+import org.springframework.core.type.classreading.MetadataReader;
+import org.springframework.core.type.classreading.MetadataReaderFactory;
+import org.springframework.core.type.classreading.SimpleMetadataReaderFactory;
 
-import java.net.URL;
-import java.util.Collection;
+import java.io.IOException;
+import java.util.HashSet;
 import java.util.Set;
-import java.util.stream.Collectors;
 
+@Slf4j
+public class EnumBeanRegistrar implements ImportBeanDefinitionRegistrar {
 
-public class EnumBeanRegistrar implements ImportBeanDefinitionRegistrar, Ordered {
+    @SneakyThrows
     @Override
-    public void registerBeanDefinitions(@NotNull AnnotationMetadata importingClassMetadata, @NotNull BeanDefinitionRegistry registry) {
-        Collection<URL> urls = ClasspathHelper.forJavaClassPath();
-        Set<URL> filteredUrls = urls.stream()
-                // Фильтруем по каким-то условиям
-                .filter(url -> !url.getFile().contains("spring") && !url.toString().contains("hibernate-") && !url.toString().contains("jakarta") && !url.toString().contains("javax") && !url.toString().contains("java"))
-                // Можно также проверять url.getPath() или url.toString()
-                .collect(Collectors.toSet());
-        Reflections reflections = new Reflections(new ConfigurationBuilder()
-                .setUrls(filteredUrls)
-                .setScanners(Scanners.TypesAnnotated));
-        Set<Class<?>> annotatedClasses = reflections.getTypesAnnotatedWith(EnumBean.class);
-        for (Class<?> clazz : annotatedClasses) {
-            if (clazz.isEnum()) {
-                boolean hasPrefix;
-                String prefix = clazz.getAnnotation(EnumBean.class).enumNamePrefix();
-                if (prefix.equals("true")) {
-                    hasPrefix = true;
-                } else if (prefix.equals("false")) {
-                    hasPrefix = false;
-                } else {
-                    throw new RuntimeException("EnumBean.enumNamePrefix must be true or false");
-                }
-                for (Object aggregate : clazz.getEnumConstants()) {
-                    Enum<?> enumConst = (Enum<?>) aggregate;
-                    RootBeanDefinition definition = new RootBeanDefinition();
-                    definition.setTargetType(clazz);
-                    definition.setInstanceSupplier(() -> enumConst);
-                    definition.setAutowireMode(AbstractBeanDefinition.AUTOWIRE_NO);
-                    registry.registerBeanDefinition((hasPrefix ? (clazz.getSimpleName() + ".") : "") + enumConst.name(), definition);
-                }
+    public void registerBeanDefinitions(AnnotationMetadata importingClassMetadata, BeanDefinitionRegistry registry) {
+        Set<Class<?>> enumClasses = findAnnotatedEnums();
+        for (Class<?> clazz : enumClasses) {
+            boolean hasPrefix;
+            String prefix = clazz.getAnnotation(EnumBean.class).enumNamePrefix();
+            if (prefix.equals("true")) {
+                hasPrefix = true;
+            } else if (prefix.equals("false")) {
+                hasPrefix = false;
+            } else {
+                throw new RuntimeException("EnumBean.enumNamePrefix must be true or false");
+            }
+            for (Object aggregate : clazz.getEnumConstants()) {
+                Enum<?> enumConst = (Enum<?>) aggregate;
+                RootBeanDefinition definition = new RootBeanDefinition();
+                definition.setTargetType(clazz);
+                definition.setInstanceSupplier(() -> enumConst);
+                definition.setAutowireMode(AbstractBeanDefinition.AUTOWIRE_NO);
+                registry.registerBeanDefinition((hasPrefix ? (clazz.getSimpleName() + ".") : "") + enumConst.name(), definition);
             }
         }
     }
 
-    @Override
-    public int getOrder() {
-        return Ordered.HIGHEST_PRECEDENCE;
+    private Set<Class<?>> findAnnotatedEnums() throws IOException {
+        Set<Class<?>> result = new HashSet<>();
+        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        MetadataReaderFactory metadataReaderFactory = new SimpleMetadataReaderFactory();
+        String packagePath = "classpath*:**/*.class";
+        for (var resource : resolver.getResources(packagePath)) {
+            try {
+                MetadataReader metadataReader = metadataReaderFactory.getMetadataReader(resource);
+                if (metadataReader.getAnnotationMetadata().hasAnnotation("dada.tuda.framework.annotations.EnumBean")) {
+                    String className = metadataReader.getClassMetadata().getClassName();
+                    Class<?> clazz = ClassLoader.getSystemClassLoader().loadClass(className);
+                    if (clazz.isEnum() && clazz.isAnnotationPresent(EnumBean.class)) {
+                        result.add(clazz);
+                    }
+                }
+            } catch (ClassNotFoundException | NoClassDefFoundError e) {
+            }
+        }
+        return result;
     }
+
+
 }
+
 
 
