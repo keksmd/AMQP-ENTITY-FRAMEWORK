edbc0c5466b1fe0f362115d2e718fca23e402e34
Алекс <lexagri200430@gmail.com>
2025-05-31 00:43:14 +0300

    Add new messaging framework components and refactor existing interfaces

--- src/main/java/dada/tuda/framework/configuration/auto/aspect/DetailedCacheEvictAspectConfiguration.java
+++ src/main/java/dada/tuda/framework/configuration/auto/aspect/DetailedCacheEvictAspectConfiguration.java
@@ -1,21 +1,17 @@
 package dada.tuda.framework.configuration.auto.aspect;
 
 import dada.tuda.framework.annotations.DetailedCacheEvictRedisAspect;
-import dada.tuda.framework.configuration.auto.redis.CustomCacheConfig;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
-@Import({ CustomCacheConfig.class })
 @Configuration
 public class DetailedCacheEvictAspectConfiguration {
     @Bean
-    @ConditionalOnBean({ RedisConnectionFactory.class, RedisCacheManager.class })
+    @ConditionalOnBean({ RedisConnectionFactory.class })
     public DetailedCacheEvictRedisAspect detailedCacheEvictAspect(CacheManager cacheManager, RedisTemplate<String, ?> redisTemplate) {
         return new DetailedCacheEvictRedisAspect(cacheManager, redisTemplate);
     }
