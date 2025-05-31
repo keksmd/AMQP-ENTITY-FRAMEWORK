bd6cb9fdaa0d13a885ff6fc7d1aa1c9048e8f446
Алекс <lexagri200430@gmail.com>
2025-05-31 04:26:43 +0300

    Refactor messaging configuration to include ConnectionFactory in bean definitions and update RabbitAdmin initialization

--- src/main/java/dada/tuda/framework/DadaTudaFrameworkProperties.java
+++ src/main/java/dada/tuda/framework/DadaTudaFrameworkProperties.java
@@ -11,6 +11,10 @@ public class DadaTudaFrameworkProperties {
     private MessagingProperties messaging;
     private DomainsProperties domains;
 
+    public DadaTudaFrameworkProperties(MessagingProperties messaging, DomainsProperties domains) {
+        this.messaging = messaging;
+        this.domains = domains;
+    }
 
     @Getter
     @Setter
