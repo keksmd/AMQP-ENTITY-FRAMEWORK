be6f4874118f40aeb649b3f26b2199620e9c400c
Алекс <lexagri200430@gmail.com>
2025-06-09 06:51:53 +0300

    Refactor configuration properties and update dependencies

--- src/main/java/dada/tuda/framework/springconf/PropertiesConfig.java
+++ src/main/java/dada/tuda/framework/springconf/PropertiesConfig.java
@@ -1,12 +1,9 @@
 package dada.tuda.framework.springconf;
 
-import dada.tuda.framework.DadaTudaFrameworkProperties;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @AutoConfiguration
-@EnableConfigurationProperties(DadaTudaFrameworkProperties.class)
 @ConfigurationPropertiesScan("dada.tuda.framework")
 public class PropertiesConfig {
 }
