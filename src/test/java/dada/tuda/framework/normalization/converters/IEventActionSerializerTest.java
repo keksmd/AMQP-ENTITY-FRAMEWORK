abc0470906752ad700e04b7ad538cf130341c4c5
Алекс <lexagri200430@gmail.com>
2025-07-21 11:44:57 +0300

    DADA-208 проблема с ObjectMapper

--- src/test/java/dada/tuda/framework/normalization/converters/IEventActionSerializerTest.java
+++ src/test/java/dada/tuda/framework/normalization/converters/IEventActionSerializerTest.java
@@ -38,6 +38,7 @@ class NormalMessageSerializerTest {
 
 
     @Qualifier("objectMapperForRabbitEntities")
+    @Autowired
     ObjectMapper defaultObjectMapper;
     @Autowired
     private MessageRepository messageRepository;
